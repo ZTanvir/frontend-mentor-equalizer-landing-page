c0d41069895db90c176aa1c15f513fb15ddaccdf
zahirul <zahirulopel@gmail.com>
2024-09-29 17:53:14 +0600

    component:add header component

--- src/components/Equalizer.jsx
+++ src/components/Equalizer.jsx
@@ -1,4 +1,11 @@
+import Header from "../components/Header";
+
 const Equalizer = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header text="equalizer" />
+    </div>
+  );
 };
+
 export default Equalizer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = ({ text }) => {
+  return (
+    <header>
+      <h1>{text}</h1>
+    </header>
+  );
+};
+export default Header;

792ad92b4fdd7c3a68e87741f8d509c4a7da5dee
zahirul <zahirulopel@gmail.com>
2024-10-02 18:29:54 +0600

    style:add footer for equalizer page

--- src/components/Equalizer.jsx
+++ src/components/Equalizer.jsx
@@ -1,12 +1,16 @@
 import Header from "../components/Header";
 import Main from "./Main";
 import styles from "../styles/equalizerpage.module.css";
+import Footer from "./Footer";
 
 const Equalizer = () => {
   return (
     <div className={styles.equalizerPage}>
       <Header text="equalizer" />
       <Main />
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import styles from "../styles/footer.module.css";
+import fbIcon from "../assets/icon-facebook.svg";
+import insIcon from "../assets/icon-instagram.svg";
+import twitterIcon from "../assets/icon-twitter.svg";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className={styles.footerContainer}>
+      <p className={styles.footerTitle}>equalizer</p>
+      <div className={styles.aboutWebsite}>
+        <p>All rights reserved © Equalizer {currentYear}</p>
+        <address>
+          Have any problems? Contact us via social media or email us at{" "}
+          <a href="equalizer@example.com">equalizer@example.com</a>{" "}
+        </address>
+      </div>
+      <div className={styles.iconContainer}>
+        <span>
+          <img src={fbIcon} alt="facebook icon" />
+        </span>
+        <span>
+          <img className={styles.insIcon} src={insIcon} alt="instragram icon" />
+        </span>
+        <span>
+          <img
+            className={styles.twitterIcon}
+            src={twitterIcon}
+            alt="twitter icon"
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
+export default Footer;

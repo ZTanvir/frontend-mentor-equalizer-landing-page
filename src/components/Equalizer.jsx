fdab765f224ef706694f51a258a9ac21beb450e5
zahirul <zahirulopel@gmail.com>
2024-10-03 19:15:47 +0600

    style:add bg pattern one to the top right of the page

--- src/components/Equalizer.jsx
+++ src/components/Equalizer.jsx
@@ -3,6 +3,7 @@ import Main from "./Main";
 import styles from "../styles/equalizerpage.module.css";
 import Footer from "./Footer";
 import mainBgImg from "../assets/bg-main-desktop.png";
+import bgPatternOneImg from "../assets/bg-pattern-1.svg";
 
 const Equalizer = () => {
   return (
@@ -15,6 +16,12 @@ const Equalizer = () => {
       <footer>
         <Footer />
       </footer>
+      <div className={styles.bgPatternOne}>
+        <img
+          src={bgPatternOneImg}
+          alt="abstract pattern to show in life top right of the website"
+        />
+      </div>
     </div>
   );
 };

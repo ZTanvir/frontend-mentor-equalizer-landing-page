60d0bc4062eda01c406d86b4746109396aadf7b7
zahirul <zahirulopel@gmail.com>
2024-10-03 19:06:13 +0600

    style:add main background image

--- src/components/Equalizer.jsx
+++ src/components/Equalizer.jsx
@@ -2,10 +2,14 @@ import Header from "../components/Header";
 import Main from "./Main";
 import styles from "../styles/equalizerpage.module.css";
 import Footer from "./Footer";
+import mainBgImg from "../assets/bg-main-desktop.png";
 
 const Equalizer = () => {
   return (
-    <div className={styles.equalizerPage}>
+    <div
+      style={{ backgroundImage: `url(${mainBgImg})` }}
+      className={styles.equalizerPage}
+    >
       <Header text="equalizer" />
       <Main />
       <footer>
